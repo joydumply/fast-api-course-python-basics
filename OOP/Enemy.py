054a74de38f7c86912c4ba183723694f877db1bc
joydumply <nick.babichev@gmail.com>
2026-01-13 19:24:24 +0400

    Begins OOP (Lesson 42); Added .gitignore

---
+++ OOP/Enemy.py
@@ -0,0 +1,4 @@
+class Enemy: 
+	type_of_enemy: str
+	hp: int = 10
+	ad: int = 1
---
+++ OOP/Main.py
@@ -0,0 +1,7 @@
+from Enemy import * 
+
+enemy = Enemy()
+enemy.type_of_enemy = 'Zombie'
+
+print(f'{enemy.type_of_enemy} has {enemy.hp} and can do an attack of {enemy.ad}')
+
