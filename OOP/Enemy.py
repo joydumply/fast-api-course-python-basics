3b7957a9d11e492060e8e682a636768a407184c6
joydumply <nick.babichev@gmail.com>
2026-01-13 20:24:09 +0400

    48. Private fields

--- OOP/Enemy.py
+++ OOP/Enemy.py
@@ -1,18 +1,22 @@
 class Enemy: 
-	type_of_enemy: str
+	__type_of_enemy: str # Made a private
 	hp: int = 10
 	ad: int = 1
 
 	def __init__(self, type_of_enemy, hp = 10, ad = 1):
-		self.type_of_enemy = type_of_enemy
+		self.__type_of_enemy = type_of_enemy
 		self.hp = hp
 		self.ad = ad
 
 	def talk (self):
-		print(f"I am a {self.type_of_enemy}. Be prepared to fight!")
+		print(f"I am a {self.__type_of_enemy}. Be prepared to fight!")
 	
 	def walk_forward(self):
-		print(f'{self.type_of_enemy} moves closer to you.')
+		print(f'{self.__type_of_enemy} moves closer to you.')
 	
 	def attack(self):
-		print(f'{self.type_of_enemy} attacks for {self.ad} damage!')
+		print(f'{self.__type_of_enemy} attacks for {self.ad} damage!')
+
+	def get_type_of_enemy(self):
+		return self.__type_of_enemy
+	
