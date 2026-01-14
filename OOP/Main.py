1ebd78b5c2dfcd02655200165f04a5f76296a461
joydumply <nick.babichev@gmail.com>
2026-01-14 19:35:43 +0400

    55. Polymorphism

--- OOP/Main.py
+++ OOP/Main.py
@@ -2,12 +2,15 @@ from Enemy import *
 from Zombie import Zombie
 from Ogre import Ogre
 
+def battle(e: Enemy):
+	e.talk()
+	e.attack()
+
 zombie = Zombie(hp = 15, ad = 3) # made a stronger zombie
 ogre = Ogre(20, 3)
 
 zombie.print_info()
 ogre.print_info()
 
-zombie.talk()
-
-ogre.talk()
+battle(zombie)
+battle(ogre)
